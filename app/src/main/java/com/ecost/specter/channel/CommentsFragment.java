caf607d9213afee324e16a8d5504ae274d5de033
F1kkus <mesropov.ya@gmail.com>
2023-02-05 18:46:47 +0300

    comments page layout created

--- app/src/main/java/com/ecost/specter/channel/CommentsFragment.java
+++ app/src/main/java/com/ecost/specter/channel/CommentsFragment.java
@@ -12,12 +12,17 @@ import com.ecost.specter.R;
 
 public class CommentsFragment extends Fragment {
 
+    ChannelActivity channelActivity;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View inflaterView = inflater.inflate(R.layout.fragment_comments, container, false);
 
+        channelActivity = (ChannelActivity) requireActivity();
 
+        inflaterView.findViewById(R.id.button_close).setOnClickListener(view -> channelActivity.getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container_view, new ChannelFragment()).commit());
 
         return inflaterView;
     }
+
 }
