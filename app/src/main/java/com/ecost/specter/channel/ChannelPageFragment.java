f4a5c1a35ac7b4034cabf7203b7d1d66401cd7f0
F1kkus <mesropov.ya@gmail.com>
2023-02-24 00:11:32 +0300

    channel category and description are now displayed on it's page

--- app/src/main/java/com/ecost/specter/channel/ChannelPageFragment.java
+++ app/src/main/java/com/ecost/specter/channel/ChannelPageFragment.java
@@ -17,8 +17,8 @@ import com.ecost.specter.R;
 
 public class ChannelPageFragment extends Fragment {
 
-    LinearLayout bBack, bChannelSettings;
-    TextView tTitle, tShortChannelLink;
+    LinearLayout bBack, bChannelSettings, lDescription;
+    TextView tTitle, tShortChannelLink, tChannelCategory, tChannelDescription;
     ChannelActivity channelActivity;
 
     @SuppressLint("SetTextI18n")
@@ -28,13 +28,24 @@ public class ChannelPageFragment extends Fragment {
 
         bBack = inflaterView.findViewById(R.id.button_back);
         tTitle = inflaterView.findViewById(R.id.channel_title);
+        tChannelCategory = inflaterView.findViewById(R.id.channel_category);
         tShortChannelLink = inflaterView.findViewById(R.id.short_channel_link);
+        tChannelDescription = inflaterView.findViewById(R.id.channel_text_description);
         bChannelSettings = inflaterView.findViewById(R.id.button_channel_settings);
+        lDescription = inflaterView.findViewById(R.id.channel_description_block);
         channelActivity = (ChannelActivity) requireActivity();
 
         tTitle.setText(channelActivity.channelTitle);
         tShortChannelLink.setText(getText(R.string.symbol_at) + channelActivity.channelShortLink);
         if (channelActivity.channelAdmin.equals(authId)) bChannelSettings.setVisibility(View.VISIBLE);
+        if (channelActivity.categoryId != 0) {
+            tChannelCategory.setText(getString(R.string.symbol_dot) + " " + getResources().getStringArray(R.array.channel_settings_array_category)[channelActivity.categoryId].toLowerCase());
+            tChannelCategory.setVisibility(View.VISIBLE);
+        }
+        if (channelActivity.channelDescription != null) {
+            tChannelDescription.setText(channelActivity.channelDescription);
+            lDescription.setVisibility(View.VISIBLE);
+        }
 
         bBack.setOnClickListener(view -> channelActivity.getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container_view, new ChannelFragment()).commit());
 
