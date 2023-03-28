035fe670890d5bff0c1de6fd799027565d7f1ff2
F1kkus <mesropov.ya@gmail.com>
2023-03-29 01:55:50 +0300

    added agree alert dialog

--- app/src/main/java/com/ecost/specter/menu/EcostSettingsMenuFragment.java
+++ app/src/main/java/com/ecost/specter/menu/EcostSettingsMenuFragment.java
@@ -4,38 +4,23 @@ import static com.ecost.specter.Routing.authEcostId;
 import static com.ecost.specter.Routing.hash;
 import static com.ecost.specter.Routing.myDB;
 import static com.ecost.specter.Routing.popup;
-import static com.ecost.specter.Routing.pushPreferenceAuth;
-import static com.ecost.specter.Routing.pushPreferenceEcostId;
-import static com.ecost.specter.Routing.pushPreferenceId;
-import static com.ecost.specter.Routing.pushPreferenceShortUserLink;
-import static com.ecost.specter.Routing.pushPreferenceUserName;
 
 import android.app.AlertDialog;
-import android.content.DialogInterface;
-import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 
-import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
 import android.text.InputFilter;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.animation.AnimationUtils;
 import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.ecost.specter.R;
-import com.ecost.specter.Routing;
-import com.ecost.specter.auth.PasswordSignUpFragment;
-import com.ecost.specter.auth.SpecterStartFragment;
-import com.ecost.specter.models.User;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.database.DataSnapshot;
 
-import java.util.Objects;
 import java.util.regex.Pattern;
 
 public class EcostSettingsMenuFragment extends Fragment {
@@ -67,17 +52,15 @@ public class EcostSettingsMenuFragment extends Fragment {
             TextView tHeader = (TextView) promptsView.findViewById(R.id.header);
             EditText eNewNumberPhone = (EditText) promptsView.findViewById(R.id.input_one);
             EditText ePass = (EditText) promptsView.findViewById(R.id.input_two);
-            TextView tButtonOk = (TextView) promptsView.findViewById(R.id.button_okay);
-            TextView tButtonCancel = (TextView) promptsView.findViewById(R.id.button_cancel);
 
-            tHeader.setText(R.string.alert_dialog_edit_number_phone_header);
-            eNewNumberPhone.setHint(R.string.alert_dialog_edit_number_phone_header_number_phone);
-            ePass.setHint(R.string.alert_dialog_edit_number_phone_header_password);
+            tHeader.setText(R.string.edit_number_phone_alert_dialog_header);
+            eNewNumberPhone.setHint(R.string.edit_number_phone_alert_dialog_hint_number_phone);
+            ePass.setHint(R.string.edit_number_phone_alert_dialog_hint_password);
 
             eNewNumberPhone.setInputType(3); // 3 - id Input type phone
             ePass.setInputType(129); // 129 - id Input type textPassword
 
-            tButtonOk.setOnClickListener(view1 -> {
+            promptsView.findViewById(R.id.button_okay).setOnClickListener(view1 -> {
                 myDB.child("ecost").child("users").child(String.valueOf(authEcostId)).child("password").get().addOnCompleteListener(taskPassword -> {
                     if (!String.valueOf(taskPassword.getResult().getValue()).equals(hash(ePass.getText().toString()))) popup(mainMenuActivity, view, 1, getString(R.string.ecost_settings_menu_error_wrong_password));
                     else {
@@ -98,9 +81,10 @@ public class EcostSettingsMenuFragment extends Fragment {
 
             AlertDialog alertDialog = mDialogBuilder.create();
 
+            alertDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
             alertDialog.show();
 
-            tButtonCancel.setOnClickListener(view1 -> alertDialog.cancel());
+            promptsView.findViewById(R.id.button_cancel).setOnClickListener(view1 -> alertDialog.cancel());
         });
 
         inflaterView.findViewById(R.id.setting_password).setOnClickListener(view -> {
@@ -114,15 +98,13 @@ public class EcostSettingsMenuFragment extends Fragment {
             TextView tHeader = (TextView) promptsView.findViewById(R.id.header);
             EditText eNewPass = (EditText) promptsView.findViewById(R.id.input_one);
             EditText eOldPass = (EditText) promptsView.findViewById(R.id.input_two);
-            TextView tButtonOk = (TextView) promptsView.findViewById(R.id.button_okay);
-            TextView tButtonCancel = (TextView) promptsView.findViewById(R.id.button_cancel);
-
-            tHeader.setText(R.string.alert_dialog_edit_password_header);
-            eNewPass.setHint(R.string.aler_dialog_edit_password_header_new_password);
-            eOldPass.setHint(R.string.aler_dialog_edit_password_header_old_password);
 
             eNewPass.setInputType(129);
             eOldPass.setInputType(129);
+
+            tHeader.setText(R.string.edit_password_alert_dialog_header);
+            eNewPass.setHint(R.string.edit_password_alert_dialog_hint_new_password);
+            eOldPass.setHint(R.string.edit_password_alert_dialog_hint_old_password);
 
             InputFilter filterPassword = (source, start, end, dest, dstart, dend) -> {
                 for (int i = start; i < end; i++) if (!Pattern.compile("^[A-ZА-Я\\d_.%+@$#!-]+$", Pattern.CASE_INSENSITIVE).matcher(String.valueOf(source.charAt(i))).find()) return "";
@@ -130,7 +112,7 @@ public class EcostSettingsMenuFragment extends Fragment {
             };
             eNewPass.setFilters(new InputFilter[] {filterPassword, new InputFilter.LengthFilter(128)});
 
-            tButtonOk.setOnClickListener(view1 -> {
+            promptsView.findViewById(R.id.button_okay).setOnClickListener(view1 -> {
                 myDB.child("ecost").child("users").child(String.valueOf(authEcostId)).child("password").get().addOnCompleteListener(taskPassword -> {
                     if (!String.valueOf(taskPassword.getResult().getValue()).equals(hash(eOldPass.getText().toString()))) popup(mainMenuActivity, view, 1, getString(R.string.ecost_settings_menu_error_wrong_password));
                     else {
@@ -143,9 +125,10 @@ public class EcostSettingsMenuFragment extends Fragment {
 
             AlertDialog alertDialog = mDialogBuilder.create();
 
+            alertDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
             alertDialog.show();
 
-            tButtonCancel.setOnClickListener(view1 -> alertDialog.cancel());
+            promptsView.findViewById(R.id.button_cancel).setOnClickListener(view1 -> alertDialog.cancel());
         });
 
         return inflaterView;
