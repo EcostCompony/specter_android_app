9ac49ad28a6402eee56f7b9ac2f89029b835cef1
tebeskakoi <getmanchuckdan1iil@gmail.com>
2023-04-06 18:30:26 +0300

    developed a search for subscribers by name or short link

--- app/src/main/java/com/ecost/specter/channel/SubscribersChannelSettingsFragment.java
+++ app/src/main/java/com/ecost/specter/channel/SubscribersChannelSettingsFragment.java
@@ -1,6 +1,8 @@
 package com.ecost.specter.channel;
 
+import static com.ecost.specter.Routing.authId;
 import static com.ecost.specter.Routing.myDB;
+import static com.ecost.specter.Routing.pluralForm;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
@@ -10,11 +12,15 @@ import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
 import com.ecost.specter.R;
+import com.ecost.specter.models.Channel;
 import com.ecost.specter.models.Post;
 import com.ecost.specter.models.User;
 import com.ecost.specter.recyclers.PostsAdapter;
@@ -25,10 +31,14 @@ import com.google.firebase.database.DatabaseError;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
+import java.util.Map;
 import java.util.Objects;
+import java.util.regex.Pattern;
 
 public class SubscribersChannelSettingsFragment extends Fragment {
 
+    EditText eSearchUser;
     RecyclerView rUsersList;
     UsersAdapter usersAdapter;
     List<User> users = new ArrayList<>();
@@ -38,6 +48,7 @@ public class SubscribersChannelSettingsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View inflaterView = inflater.inflate(R.layout.fragment_subscribers_channel_settings, container, false);
 
+        eSearchUser = inflaterView.findViewById(R.id.input_search_user);
         rUsersList = inflaterView.findViewById(R.id.recycler_users_list);
         channelActivity = (ChannelActivity) requireActivity();
 
@@ -59,6 +70,36 @@ public class SubscribersChannelSettingsFragment extends Fragment {
             @Override public void onCancelled(@NonNull DatabaseError databaseError) {}
         };
         myDB.child("specter").child("channels").child(String.valueOf(channelActivity.channelId)).child("subscribers").addChildEventListener(childEventListener);
+
+        eSearchUser.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                users.clear();
+                ChildEventListener childEventListenerB = new ChildEventListener() {
+                    @SuppressLint("NotifyDataSetChanged")
+                    @Override
+                    public void onChildAdded(@NonNull DataSnapshot dataSnapshot, String previousChildName) {
+                        User user = Objects.requireNonNull(dataSnapshot.getValue(User.class));
+                        if (!eSearchUser.getText().toString().trim().equals("")) myDB.child("specter").child("channels").child(String.valueOf(channelActivity.channelId)).child("subscribers").removeEventListener(childEventListener);
+                        if (!eSearchUser.getText().toString().trim().equals("") && (Pattern.compile(eSearchUser.getText().toString().trim(), Pattern.CASE_INSENSITIVE).matcher(user.name.toLowerCase()).find() || Pattern.compile(eSearchUser.getText().toString().trim(), Pattern.CASE_INSENSITIVE).matcher(user.link.toLowerCase()).find())) {
+                            users.add(user);
+                        } else if (eSearchUser.getText().toString().trim().equals("")) {
+                            myDB.child("specter").child("channels").child(String.valueOf(channelActivity.channelId)).child("subscribers").addChildEventListener(childEventListener);
+                        }
+                        usersAdapter.notifyDataSetChanged();
+                    }
+
+                    @Override public void onChildChanged(@NonNull DataSnapshot dataSnapshot, String previousChildName) {}
+                    @Override public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {}
+                    @Override public void onChildMoved(@NonNull DataSnapshot dataSnapshot, String previousChildName) { }
+                    @Override public void onCancelled(@NonNull DatabaseError databaseError) { }
+                };
+                myDB.child("specter").child("channels").child(String.valueOf(channelActivity.channelId)).child("subscribers").addChildEventListener(childEventListenerB);
+            }
+
+            @Override public void onTextChanged(CharSequence s, int start, int before, int count) {}
+            @Override public void afterTextChanged(Editable s) {}
+        });
 
         return inflaterView;
     }
